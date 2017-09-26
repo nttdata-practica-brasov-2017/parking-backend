01c81c39c80089ea0a7d87e22251589715c85839
Linda Szasz <lindas_bv@yahoo.com>
2017-09-26 11:15:44 +0300

    Change data source to cloud postgres instance

--- src/main/java/ro/nttdata/bv/parking/config/AppConfig.java
+++ src/main/java/ro/nttdata/bv/parking/config/AppConfig.java
@@ -1,9 +1,11 @@
 package ro.nttdata.bv.parking.config;
 
+import org.postgresql.jdbc2.optional.SimpleDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -39,8 +41,12 @@ public class AppConfig {
 
     @Bean
     public DataSource dataSource() {
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        return builder.setType(EmbeddedDatabaseType.H2).build();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/parkingdb");
+        dataSource.setUsername( "parkinguser" );
+        dataSource.setPassword( "1q2w3e" );
+        return dataSource;
     }
 
     @Bean
